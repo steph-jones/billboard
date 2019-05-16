8f2d7b5bcbe6a592636ef05cada93dc6ab43f0ac
--replace-all <se.jones4@gmail.com>
2019-05-16 14:16:56 -0600

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'boards/index'
+  get 'boards/show'
+  get 'boards/new'
+  get 'boards/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
